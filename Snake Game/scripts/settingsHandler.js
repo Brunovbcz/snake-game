7f3dbd703407fceead19dd09311d26dd69a62817
Brunovbcz <Brunovbcz9@gmail.com>
2025-04-24 17:15:03 -0300

    Versão 1

---
+++ Snake Game/scripts/settingsHandler.js
@@ -0,0 +1,20 @@
+let gridButton = document.querySelector('#grid')
+let gameOptionsButton = document.querySelector('#game-options')
+let soundButton = document.querySelector('#sound')
+let screenButton = document.querySelector('#screen')
+
+gridButton.addEventListener('click', () => {
+    
+})
+
+gameOptionsButton.addEventListener('click', () => {
+    
+})
+
+soundButton.addEventListener('click', () => {
+    
+})
+
+screenButton.addEventListener('click', () => {
+    
+})
